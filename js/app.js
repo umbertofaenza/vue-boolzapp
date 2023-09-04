cfb50fc196eaea12744b11a949fb61fa5588f96b
umbertofaenza <umberto.faenza@gmail.com>
2023-09-04 12:08:53 +0200

    feat: add method to send msg (@enter)

--- js/app.js
+++ js/app.js
@@ -5,7 +5,25 @@ createApp({
     return {
       contacts,
       activeChatIndex: 0,
+      newMessage: {
+        date: "",
+        message: "",
+        status: "sent",
+      },
     };
+  },
+
+  methods: {
+    addMsg() {
+      const newMessageCopy = {
+        ...this.newMessage,
+      };
+
+      if (this.newMessage.message.length > 0) {
+        this.contacts[this.activeChatIndex].messages.push(newMessageCopy);
+        this.newMessage.message = "";
+      }
+    },
   },
 
   created() {
